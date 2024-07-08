10cb3144e40a21630f7dc34ba392e78b90960fd5
Tomas <talk.gtg@gmail.com>
2024-07-08 14:29:04 -0300

    Manage route for preview

--- src/contains/constants.ts
+++ src/contains/constants.ts
@@ -1,4 +1,4 @@
-export const IS_PROD = process.env.PROD || process.env.NODE_ENV === "production";
+export const IS_PROD = process.env.PROD;
 export const API_URL = process.env.NEXT_PUBLIC_MSK_WP_API;
 export const PROD_BASE_URL = process.env.NEXT_PUBLIC_PUBLIC_URL;
 export const DEV_BASE_URL = process.env.NEXT_PUBLIC_PUBLIC_URL_DEV;
