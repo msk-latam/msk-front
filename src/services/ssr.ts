155ab53b9e658d2c1f30b18fca8e49074fd744d1
Tomas <talk.gtg@gmail.com>
2024-07-11 09:40:21 -0300

    add mode cors

--- src/services/ssr.ts
+++ src/services/ssr.ts
@@ -371,6 +371,7 @@ class ApiSSRService {
     try {
       const response = await fetch(`${baseUrl}/api/signup`, {
         method: "POST",
+        mode: 'cors',
         headers: {
           "Content-Type": "application/json",
           "Accept": "application/json",
