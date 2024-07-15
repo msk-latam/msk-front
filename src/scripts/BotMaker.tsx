5290fea489235653a578c286c4b7dfc1104734ec
Tomas <talk.gtg@gmail.com>
2024-07-15 18:51:10 -0300

    Remove cl

--- src/scripts/BotMaker.tsx
+++ src/scripts/BotMaker.tsx
@@ -1,28 +1,29 @@
-'use client'
-import {useEffect, useState} from 'react';
+"use client";
+import { useEffect, useState } from "react";
 
-import { usePathname } from 'next/navigation'
+import { usePathname } from "next/navigation";
 const BotMaker = () => {
-    const pathname = usePathname()
-    const [mountedBot, setMountedBot] = useState(false);
+  const pathname = usePathname();
+  const [mountedBot, setMountedBot] = useState(false);
 
-    useEffect(() => {
-        const countries = ["ar", "cl", "ec"];
+  useEffect(() => {
+    const countries = ["ar", "ec"];
 
-        const onValidCountry = pathname.split("/").some(part => countries.includes(part));
+    const onValidCountry = pathname
+      .split("/")
+      .some((part) => countries.includes(part));
 
-        if (!onValidCountry && !mountedBot) {
-            const script = document.createElement('script');
-            script.type = 'text/javascript';
-            script.async = true;
-            script.src = 'https://go.botmaker.com/rest/webchat/p/XG5DC3KZSF/init.js';
-            document.body.appendChild(script);
-            setMountedBot(true)
-        }
+    if (!onValidCountry && !mountedBot) {
+      const script = document.createElement("script");
+      script.type = "text/javascript";
+      script.async = true;
+      script.src = "https://go.botmaker.com/rest/webchat/p/XG5DC3KZSF/init.js";
+      document.body.appendChild(script);
+      setMountedBot(true);
+    }
+  }, []);
 
-    }, []);
-
-    return null;
+  return null;
 };
 
 export default BotMaker;
