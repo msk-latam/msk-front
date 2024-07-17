2cd453e700b2abe47ff59b8bb2d8ab1ea21a7693
thomasloader1 <talk.gtg@gmail.com>
2024-07-17 12:43:09 -0300

    Add possible headers for cors

--- next.config.js
+++ next.config.js
@@ -5,25 +5,22 @@ const nextConfig = {
   async headers() {
         return [
             {
-                // matching all API routes
-                source: "https://dev.msklatam.tech/msk-laravel/public/api/:path*",
+                source: '/(.*)',
                 headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-                ]
+                    {
+                        key: 'Access-Control-Allow-Origin',
+                        value: '*', // Update this with your frontend URL
+                    },
+                    {
+                        key: 'Access-Control-Allow-Methods',
+                        value: 'GET, POST, PUT, DELETE, OPTIONS',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Headers',
+                        value: 'X-Requested-With, Content-Type, Authorization',
+                    },
+                ],
             },
-            {
-                // matching all API routes
-                source: "https://laravel-api.msklatam.com/msk-laravel/public/api/:path*",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-                ]
-            }
         ]
     },
   // These are all the locales you want to support in
