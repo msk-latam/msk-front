b39b518312332b77b0d48194592b672b348e8b70
Tomas <talk.gtg@gmail.com>
2024-07-22 16:00:23 -0300

    Icons cards

--- src/components/Containers/profile/ProductAccountButton.tsx
+++ src/components/Containers/profile/ProductAccountButton.tsx
@@ -1,14 +1,10 @@
-import {UserCourseProgress} from "@/data/types";
-import {
-  getStatusIcon,
-  statusCourse,
-  statusOrdenVenta,
-} from "@/lib/account";
-import React, {FC, useState} from "react";
-import Link from "next/link";
-import CancelTrialModal from "@/components/Modal/CancelTrial";
-import ButtonActivateOrRegister from "@/components/Account/ButtonActivateOrRegister";
-import Image from "next/image";
+import { UserCourseProgress } from '@/data/types';
+import { getStatusIcon, statusCourse, statusOrdenVenta } from '@/lib/account';
+import React, { FC, useState } from 'react';
+import Link from 'next/link';
+import CancelTrialModal from '@/components/Modal/CancelTrial';
+import ButtonActivateOrRegister from '@/components/Account/ButtonActivateOrRegister';
+import Image from 'next/image';
 
 interface ProductAccountButtonProps {
   product: UserCourseProgress;
@@ -18,54 +14,62 @@ interface ProductAccountButtonProps {
 }
 
 const ProductAccountButton: FC<ProductAccountButtonProps> = ({
-                                                               product,
-                                                               onRequest,
-                                                               isRunning,
-                                                               onClick,
-                                                             }) => {
-  const [showCancelTrial, setShowCancelTrial] = useState(false)
-  const {status} = product;
-  const {isDisabled} = statusCourse(status);
+  product,
+  onRequest,
+  isRunning,
+  onClick,
+}) => {
+  const [showCancelTrial, setShowCancelTrial] = useState(false);
+  const { status } = product;
+  const { isDisabled } = statusCourse(status);
   const statusOV = statusOrdenVenta(product?.ov, status);
-  const textStatus = statusOV.isDisabled ? statusOV.disabledText : statusOV.hasText
-  const iconStatus = getStatusIcon(textStatus, product?.ov);
+  const textStatus = statusOV.isDisabled
+    ? statusOV.disabledText
+    : statusOV.hasText;
+  const iconStatus = getStatusIcon(textStatus);
+  console.log({ iconStatus });
 
   return (
-    <div className="product-button-wrp">
-      <div className="product-button-footer text-grey-course">
-        <div className="coursee-clock">
-          <Image
-            {...iconStatus}
-            alt={textStatus as string}
-          />
-          <span className="ml-2">
-          {textStatus === "" ? status : textStatus}
-            {statusOV.hasText === 'Prueba' &&
-              <div className="ml-1 inline-block">
-                - <Link href="#"
-                        onClick={() => setShowCancelTrial(true)}
-                        className="underline text-violet-custom hover:text-violet-custom">
-                Dar de baja
-              </Link>
+    <div className='product-button-wrp'>
+      <div className='product-button-footer text-grey-course'>
+        <div className='coursee-clock'>
+          <Image {...iconStatus} alt={textStatus as string} />
+          <span className='ml-2'>
+            {textStatus === '' ? status : textStatus}
+            {statusOV.hasText === 'Prueba' && (
+              <div className='ml-1 inline-block'>
+                -{' '}
+                <Link
+                  href='#'
+                  onClick={() => setShowCancelTrial(true)}
+                  className='underline text-violet-custom hover:text-violet-custom'
+                >
+                  Dar de baja
+                </Link>
               </div>
-            }
-        </span>
+            )}
+          </span>
         </div>
         <ButtonActivateOrRegister
           isDisabledActivate={isDisabled || statusOV.isDisabled}
           handleActivateClick={onClick}
-          whenActivate={onRequest || isRunning || (typeof status === 'string' && status.includes("Listo para enrolar"))}
+          whenActivate={
+            onRequest ||
+            isRunning ||
+            (typeof status === 'string' &&
+              status.includes('Listo para enrolar'))
+          }
           status={status}
           productSlug={product.slug}
         />
       </div>
 
       <CancelTrialModal
-        isOpenProp={showCancelTrial} item={product}
+        isOpenProp={showCancelTrial}
+        item={product}
         onCloseModal={() => setShowCancelTrial(false)}
       />
     </div>
-
   );
 };
 
