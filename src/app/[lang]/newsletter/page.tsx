195be2b04a393a01e4a17cc64c15e75f87918afe
Ariel-Eitner-MSK <arieleitner@msklatam.com>
2024-07-29 15:16:24 -0300

    fixed layout

--- src/app/[lang]/newsletter/page.tsx
+++ src/app/[lang]/newsletter/page.tsx
@@ -1,26 +1,18 @@
 'use client';
 import FooterNewsletter from '@/components/Footer/Newsletter';
+import LayoutPage from '@/components/MSK/LayoutPage';
 import { FC } from 'react';
 const newsletter: FC = () => {
   return (
     <>
       <div className=''>
-        <div className='bg-content-msk nc-HeadBackgroundCommon'>
-          <div className='container relative pt-10 pb-16 lg:pt-20 lg:pb-28'>
-            <header className='text-center max-w-2xl mx-auto'>
-              <h2 className='flex items-center text-3xl leading-[115%] md:text-5xl md:leading-[115%] font-bold text-neutral-900 dark:text-neutral-100 justify-center'>
-                Nuestro Newsletter
-              </h2>
-              <span className='block text-sm mt-2 text-[#6474a6] sm:text-base dark:text-neutral-200 max-w-[70%] md:max-w-[90%] mx-auto'>
-                Suscrí­bete para acceder a descuentos exclusivos, becas
-                completas y contenido personalizado
-              </span>
-            </header>
-            <div className='p-5 mx-auto bg-white rounded-[12px] sm:rounded-[40px] shadow-lg sm:p-10 mt-10 lg:mt-20 lg:p-16 dark:bg-neutral-900'>
-              <FooterNewsletter email='' setShow={() => {}} />
-            </div>
-          </div>
-        </div>
+        <LayoutPage
+          subHeading='Suscrí­bete para acceder a descuentos exclusivos, becas
+                  completas y contenido personalizado'
+          heading='Nuestro Newsletter'
+        >
+          <FooterNewsletter email='' setShow={() => {}} />
+        </LayoutPage>
       </div>
     </>
   );
