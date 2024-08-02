d89c2873e9919d0eb18edbb56c7df49443863c82
Ariel-Eitner-MSK <arieleitner@msklatam.com>
2024-08-02 13:59:54 -0300

    link

--- src/components/Account/ButtonActivateOrRegister.tsx
+++ src/components/Account/ButtonActivateOrRegister.tsx
@@ -1,42 +1,55 @@
-
-import { FC } from 'react'
-import {useRouter} from "next/navigation";
-import {hasText} from "@/lib/account";
-
+import { FC, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { hasText } from '@/lib/account';
 
 interface ButtonActivateOrRegisterProps {
-    isDisabledActivate: boolean;
-    handleActivateClick: () => void;
-    whenActivate: boolean;
-    status: string;
-    productSlug: string | undefined;
+  isDisabledActivate: boolean;
+  handleActivateClick: () => void;
+  whenActivate: boolean;
+  status: string;
+  productSlug: string | undefined;
 }
 
-const ButtonActivateOrRegister: FC<ButtonActivateOrRegisterProps> = ({ isDisabledActivate, handleActivateClick, whenActivate, status, productSlug }) => {
-    const router = useRouter();
-    return (
-        <>
-            {isDisabledActivate ?  <button
-                className="course-network text-primary font-bold disabled:text-grey-disabled disabled:cursor-not-allowed disabled:opacity-70"
-                onClick={() => router.push(`/curso/${productSlug}`)}
-            >
-                Inscríbete
-            </button> : <button
-                className="course-network text-primary font-bold disabled:text-grey-disabled disabled:cursor-not-allowed disabled:opacity-70"
-                onClick={handleActivateClick}
-                disabled={isDisabledActivate}
-            >
-                {whenActivate ? (
-                    <div className="flex justify-center items-center">
-                        Activando...
-                    </div>
-                ) : (
-                    hasText(status)
-                )}
-            </button>
-            }
-        </>
-    )
-}
+const ButtonActivateOrRegister: FC<ButtonActivateOrRegisterProps> = ({
+  isDisabledActivate,
+  handleActivateClick,
+  whenActivate,
+  status,
+  productSlug,
+}) => {
+  const router = useRouter();
 
-export default ButtonActivateOrRegister
+  useEffect(() => {
+    console.log('Component re-rendered');
+    console.log('isDisabledActivate:', isDisabledActivate);
+    console.log('whenActivate:', whenActivate);
+    console.log('status:', status);
+    console.log('productSlug:', productSlug);
+  }, [isDisabledActivate, whenActivate, status, productSlug]);
+  return (
+    <>
+      {isDisabledActivate ? (
+        <button
+          className='course-network text-primary font-bold disabled:text-grey-disabled disabled:cursor-not-allowed disabled:opacity-70'
+          onClick={() => router.push(`/curso/${productSlug}`)}
+        >
+          Inscríbete
+        </button>
+      ) : (
+        <button
+          className='course-network text-primary font-bold disabled:text-grey-disabled disabled:cursor-not-allowed disabled:opacity-70'
+          onClick={handleActivateClick}
+          disabled={isDisabledActivate}
+        >
+          {whenActivate ? (
+            <div className='flex justify-center items-center'>Activando...</div>
+          ) : (
+            hasText(status)
+          )}
+        </button>
+      )}
+    </>
+  );
+};
+
+export default ButtonActivateOrRegister;
--- src/components/Containers/profile/ProductAccountButton.tsx
+++ src/components/Containers/profile/ProductAccountButton.tsx
@@ -19,6 +19,7 @@ const ProductAccountButton: FC<ProductAccountButtonProps> = ({
   isRunning,
   onClick,
 }) => {
+  console.log('product account button', product);
   const [showCancelTrial, setShowCancelTrial] = useState(false);
   const { status } = product;
   const { isDisabled } = statusCourse(status);
@@ -27,6 +28,8 @@ const ProductAccountButton: FC<ProductAccountButtonProps> = ({
     ? statusOV.disabledText
     : statusOV.hasText;
   const iconStatus = getStatusIcon(textStatus);
+
+  console.log('status', status);
 
   return (
     <div className='product-button-wrp'>
@@ -55,10 +58,10 @@ const ProductAccountButton: FC<ProductAccountButtonProps> = ({
           whenActivate={
             onRequest ||
             isRunning ||
-            (typeof status === 'string' &&
-              status.includes('Listo para enrolar'))
+            (typeof textStatus === 'string' &&
+              textStatus.includes('Listo para enrolar'))
           }
-          status={status}
+          status={textStatus}
           productSlug={product.slug}
         />
       </div>
