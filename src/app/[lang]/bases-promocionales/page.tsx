934824754d0932514dfa9cd3407ac46670241c0a
Ariel-Eitner-MSK <arieleitner@msklatam.com>
2024-08-02 14:17:12 -0300

    Update page.tsx

--- src/app/[lang]/bases-promocionales/page.tsx
+++ src/app/[lang]/bases-promocionales/page.tsx
@@ -1,31 +1,18 @@
-<<<<<<< Updated upstream
-import React, { FC } from "react";
-import PageBasesPromocionalesComponent from "@/components/MSK/bases-promocionales/Page";
-import { IS_PROD } from "@/contains/constants";
-export async function generateMetadata() {
-  return {
-    title: "Bases promocionales | MSK",
-    robots: IS_PROD
-=======
 import React, { FC } from 'react';
 import PageBasesPromocionalesComponent from '@/components/MSK/bases-promocionales/Page';
-import { isProduction } from '@/utils/isProduction';
+import { IS_PROD } from '@/contains/constants';
+
 export async function generateMetadata() {
   return {
     title: 'Bases promocionales | MSK',
-    robots: isProduction
->>>>>>> Stashed changes
+    robots: IS_PROD
       ? {
           index: true,
           follow: true,
         }
       : undefined,
     description:
-<<<<<<< Updated upstream
-      "Consulta las bases promocionales de MSK para conocer todos los detalles de nuestras promociones.",
-=======
       'Consulta las bases promocionales de MSK para conocer todos los detalles de nuestras promociones.',
->>>>>>> Stashed changes
   };
 }
 const PageContractConditions: FC = () => {
