2affd9bd5909d8390ac231ad01886cf8db0140e3
Ariel-Eitner-MSK <arieleitner@msklatam.com>
2024-08-13 12:37:00 -0300

    botmaker argentina

--- src/scripts/BotMaker.tsx
+++ src/scripts/BotMaker.tsx
@@ -1,25 +1,17 @@
 'use client';
 import { useEffect, useState } from 'react';
-
-import { usePathname } from 'next/navigation';
 const BotMaker = () => {
-  const pathname = usePathname();
   const [mountedBot, setMountedBot] = useState(false);
 
   useEffect(() => {
-    const countries = [''];
-
-    const onValidCountry = pathname
-      .split('/')
-      .some(part => countries.includes(part));
-
-    if (!onValidCountry && !mountedBot) {
+    if (!mountedBot) {
       const script = document.createElement('script');
       script.type = 'text/javascript';
       script.async = true;
       script.src = 'https://go.botmaker.com/rest/webchat/p/XG5DC3KZSF/init.js';
       document.body.appendChild(script);
       setMountedBot(true);
+      console.log('iniciando botmaker argentina');
     }
   }, []);
 
