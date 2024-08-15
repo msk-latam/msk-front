ccdbd6dc6978141a6ff91968771e9e00251798f1
Ariel-Eitner-MSK <arieleitner@msklatam.com>
2024-08-15 12:10:22 -0300

    botmaker

--- src/scripts/BotMaker.tsx
+++ src/scripts/BotMaker.tsx
@@ -1,23 +1,14 @@
-"use client";
-import { useEffect, useState } from "react";
-
-import { usePathname } from "next/navigation";
+'use client';
+import { useEffect, useState } from 'react';
 const BotMaker = () => {
-  const pathname = usePathname();
   const [mountedBot, setMountedBot] = useState(false);
 
   useEffect(() => {
-    const countries = ["ar"];
-
-    const onValidCountry = pathname
-      .split("/")
-      .some((part) => countries.includes(part));
-
-    if (!onValidCountry && !mountedBot) {
-      const script = document.createElement("script");
-      script.type = "text/javascript";
+    if (!mountedBot) {
+      const script = document.createElement('script');
+      script.type = 'text/javascript';
       script.async = true;
-      script.src = "https://go.botmaker.com/rest/webchat/p/XG5DC3KZSF/init.js";
+      script.src = 'https://go.botmaker.com/rest/webchat/p/XG5DC3KZSF/init.js';
       document.body.appendChild(script);
       setMountedBot(true);
     }
