9ef99668b647ef73698e6b5e77b4dc09ed50e790
Ariel-Eitner-MSK <arieleitner@msklatam.com>
2024-08-20 16:48:45 -0300

    prefetch mi-perfil

--- src/components/MSK/iniciar-sesion/Page.tsx
+++ src/components/MSK/iniciar-sesion/Page.tsx
@@ -1,81 +1,22 @@
 'use client';
-import { FC, useContext, useRef, useState } from 'react';
+import { FC, useState } from 'react';
+import LayoutPage from '@/components/MSK/LayoutPage';
 import ButtonPrimary from '@/components/Button/ButtonPrimary';
 import NcLink from '@/components/NcLink/NcLink';
-import { AuthContext } from '@/context/user/AuthContext';
-import { ErrorMessage, Field, Form, FormikProvider, useFormik } from 'formik';
-import * as Yup from 'yup';
-import { useGoogleReCaptcha } from 'react-google-recaptcha-v3';
-import api from '@/services/api';
-import LayoutPage from '@/components/MSK/LayoutPage';
-import { useRouter } from 'next/navigation';
-import NcImage from '@/components/NcImage/NcImage';
 import ShowErrorMessage from '@/components/ShowErrorMessage';
+
+import { useLoginForm } from './useLoginForm';
+import { FormikProvider, Form } from 'formik';
+import LoginInput from './LoginInputField';
 
 export interface PageLoginProps {
   className?: string;
-  lang?: string;
 }
-export const runtime = 'edge';
 
 const PageLogin: FC<PageLoginProps> = ({ className = '' }) => {
-  // @ts-ignore
-  const router = useRouter();
-  const { executeRecaptcha } = useGoogleReCaptcha();
-  const [loginError, setLoginError] = useState<string>('');
+  const { formik, loginError, onRequest, formRef, setLoginError } =
+    useLoginForm();
   const [showPassword, setShowPassword] = useState<boolean>(false);
-  const [onRequest, setOnRequest] = useState<boolean>(false);
-  const { state, dispatch } = useContext(AuthContext);
-  const formRef = useRef<HTMLFormElement>(null);
-  const initialValues = {
-    email: '',
-    password: '',
-    recaptcha_token: '',
-  };
-  // const history = useHistory();
-  const validationSchema = Yup.object().shape({
-    email: Yup.string()
-      .email('Correo electrónico inválido')
-      .required('El correo electrónico es requerido'),
-    password: Yup.string().required('La contraseña es requerida'),
-  });
-  const changeRoute = (newRoute: string): void => {
-    router.push(newRoute);
-  };
-  const formik = useFormik({
-    initialValues,
-    validationSchema,
-    onSubmit: async (values: any) => {
-      setOnRequest(true);
-      try {
-        if (executeRecaptcha) {
-          const formData = {
-            ...values,
-            recaptcha_token: await executeRecaptcha('login'),
-          };
-          const { data, status } = await api.postLogin(formData);
-          if (status == 200) {
-            const { name, speciality, ...restData } = data;
-            const loginData = {
-              ...restData,
-              email: formik.values.email,
-              user: { name, speciality },
-            };
-
-            dispatch({ type: 'LOGIN', payload: loginData });
-            changeRoute('/mi-perfil');
-          } else {
-            console.log(data);
-            setLoginError(data?.message as string);
-          }
-        }
-      } catch (error) {
-        console.error('Error al ejecutar reCAPTCHA:', error);
-      } finally {
-        setOnRequest(false);
-      }
-    },
-  });
 
   return (
     <div
@@ -95,47 +36,20 @@ const PageLogin: FC<PageLoginProps> = ({ className = '' }) => {
               autoComplete='off'
               ref={formRef}
             >
-              <div className='form-input-std'>
-                <label className='text-neutral-800 dark:text-neutral-200 mb-1'>
-                  E-mail
-                </label>
-                <ErrorMessage name='email' component='span' className='error' />
-                <Field type='text' name='email' placeholder='Ingresar e-mail' />
-              </div>
-              <div className='form-input-std my-4'>
-                <label className='flex justify-between items-center text-neutral-800 dark:text-neutral-200'>
-                  Contraseña
-                  <NcLink href='/recuperar' className='text-sm'>
-                    ¿Olvidaste tu contraseña?
-                  </NcLink>
-                </label>
-                <ErrorMessage
-                  name='password'
-                  component='span'
-                  className='error'
-                />
-
-                <div className='relative'>
-                  <NcImage
-                    src={
-                      showPassword
-                        ? '/images/icons/eye-solid.svg'
-                        : '/images/icons/eye-slash-solid.svg'
-                    }
-                    className='absolute top-1/2 right-4 transform -translate-y-1/2 cursor-pointer'
-                    alt='Toggle show password'
-                    width='21'
-                    height='21'
-                    onClick={() => setShowPassword(!showPassword)}
-                  />
-                  <Field
-                    type={showPassword ? 'text' : 'password'}
-                    name='password'
-                    placeholder='Ingresar contraseña'
-                    className='w-full'
-                  />
-                </div>
-              </div>
+              <LoginInput
+                label='E-mail'
+                name='email'
+                placeholder='Ingresar e-mail'
+              />
+              <LoginInput
+                label='Contraseña'
+                name='password'
+                type='password'
+                placeholder='Ingresar contraseña'
+                showPassword={showPassword}
+                toggleShowPassword={() => setShowPassword(!showPassword)}
+                isPassword
+              />
               <ButtonPrimary
                 type='submit'
                 className='w-full'
---
+++ src/components/MSK/iniciar-sesion/useLoginForm.ts
@@ -0,0 +1,73 @@
+import { useState, useContext, useRef, useEffect } from 'react';
+import { useFormik } from 'formik';
+import { AuthContext } from '@/context/user/AuthContext';
+import { useGoogleReCaptcha } from 'react-google-recaptcha-v3';
+import * as Yup from 'yup';
+import api from '@/services/api';
+import { useRouter } from 'next/navigation';
+import { cookies } from 'next/headers';
+import { CountryContext } from '@/context/country/CountryContext';
+
+export const useLoginForm = () => {
+  const router = useRouter();
+  const { executeRecaptcha } = useGoogleReCaptcha();
+  const { state, dispatch } = useContext(AuthContext);
+  const [loginError, setLoginError] = useState<string>('');
+  const [onRequest, setOnRequest] = useState<boolean>(false);
+  const formRef = useRef<HTMLFormElement>(null);
+  const { countryState: countryState } = useContext(CountryContext);
+  console.log(countryState.country);
+
+  useEffect(() => {
+    router.prefetch(`/${countryState.country}/mi-perfil`);
+  }, []);
+
+  const initialValues = {
+    email: '',
+    password: '',
+    recaptcha_token: '',
+  };
+
+  const validationSchema = Yup.object().shape({
+    email: Yup.string()
+      .email('Correo electrónico inválido')
+      .required('El correo electrónico es requerido'),
+    password: Yup.string().required('La contraseña es requerida'),
+  });
+
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit: async values => {
+      setOnRequest(true);
+      try {
+        if (executeRecaptcha) {
+          const formData = {
+            ...values,
+            recaptcha_token: await executeRecaptcha('login'),
+          };
+          const { data, status } = await api.postLogin(formData);
+          if (status == 200) {
+            const { name, speciality, ...restData } = data;
+            const loginData = {
+              ...restData,
+              email: formik.values.email,
+              user: { name, speciality },
+            };
+
+            dispatch({ type: 'LOGIN', payload: loginData });
+            router.push('/mi-perfil');
+          } else {
+            setLoginError(data?.message as string);
+          }
+        }
+      } catch (error) {
+        console.error('Error al ejecutar reCAPTCHA:', error);
+      } finally {
+        setOnRequest(false);
+      }
+    },
+  });
+
+  return { formik, loginError, onRequest, formRef, setLoginError };
+};
