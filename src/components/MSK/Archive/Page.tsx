e0007f8da9d0ee17a6e430f8a48c95198d8e4937
Ariel-Eitner-MSK <arieleitner@msklatam.com>
2024-08-27 15:35:59 -0300

    breadcrum blog

--- src/components/MSK/Archive/Page.tsx
+++ src/components/MSK/Archive/Page.tsx
@@ -18,6 +18,7 @@ import LoaderImage from '@/components/MSK/Loader/Image';
 import ItemSkeleton from '@/components/Skeleton/ItemSkeleton';
 import StorePagination from '@/components/MSK/Store/StorePagination';
 import { slugifySpecialty } from '@/lib/Slugify';
+import Breadcrum from '@/components/Breadcrum/Breadcrum';
 
 export const runtime = 'edge';
 
@@ -207,6 +208,7 @@ const PageArchiveComponent: FC<PageArchiveProps> = ({
             ) : null}
           </>
         )}
+        <div>{/* <Breadcrum /> */}</div>
         <div className=' my-10 animate-fade-down'>
           <div className='flex space-between flex-wrap mb-8 gap-2'>
             <ArchiveFilterListBox
