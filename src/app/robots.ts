f3a1bc382647b871e044663b960e6436b1f8580a
Ariel-Eitner-MSK <arieleitner@msklatam.com>
2024-09-03 14:24:22 -0300

    robots host

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,10 +1,13 @@
 import { MetadataRoute } from 'next';
 
-export default function robots(): MetadataRoute.Robots {
-  const IS_PROD = process.env.PROD || process.env.NODE_ENV === 'production';
-  console.log(process.env.PROD, 'PROD');
-  console.log(process.env.NODE_ENV, 'NODE_ENV');
+export default function robots(req: any): MetadataRoute.Robots {
+  // Obtener el dominio desde la cabecera 'host' del request
+  const host = req?.headers?.host || '';
+  const IS_PROD = host.includes('.com');
+
+  console.log(host, 'HOST');
   console.log(IS_PROD, 'IS_PROD');
+
   return {
     rules: IS_PROD
       ? {
