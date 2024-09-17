753319ff665fc83b5f94deac02e0ae33d30757cd
Ariel-Eitner-MSK <arieleitner@msklatam.com>
2024-09-17 16:43:32 -0300

    trial mercadopago

--- src/services/ssr.ts
+++ src/services/ssr.ts
@@ -715,7 +715,7 @@ class ApiSSRService {
   async callDraftContractApi(body: object) {
     try {
       const response = await fetch(
-        `http://msk-web-crm/api/zoho/sales_order/create_contract`,
+        `https://msk-web-crm/api/zoho/sales_order/create_contract`,
         // `http://crm.msklatam.net/api/zoho/sales_order/create_contract`,
         // `http://127.0.0.1:8000/api/gateway/api/mercadopago/arg/our_test`,
         {
