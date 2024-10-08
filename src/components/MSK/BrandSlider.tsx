2d36aee57005c9ad95fdfead8904b9d283966a10
Ariel-Eitner-MSK <arieleitner@msklatam.com>
2024-10-08 16:45:30 -0300

    hotjar, euneiz home

--- src/components/MSK/BrandSlider.tsx
+++ src/components/MSK/BrandSlider.tsx
@@ -6,46 +6,46 @@ import 'swiper/css/bundle';
 import Image from 'next/image';
 
 interface BrandSliderProps {
-  country: string;
+	country: string;
 }
 
 const BrandSlider: FC<BrandSliderProps> = ({ country }) => {
-  const brands = BRANDS_BY_COUNTRY[country] || BRANDS_BY_COUNTRY['default'];
-  return (
-    <div className='swiper-container'>
-      <Swiper
-        pagination
-        a11y={{ enabled: true }}
-        slidesPerView={3}
-        breakpoints={{
-          320: {
-            slidesPerView: 1,
-          },
-          480: {
-            slidesPerView: 2,
-          },
-          770: {
-            slidesPerView: 3,
-          },
-          991: {
-            slidesPerView: 3,
-          },
-        }}
-        autoplay={{
-          delay: 2000,
-          disableOnInteraction: true,
-        }}
-      >
-        {brands?.map((brand, i) => (
-          <SwiperSlide key={i} className='w-100 bg-primary'>
-            <div className='brand-container'>
-              <Image src={brand.img} alt='img not found' width={brand.width} />
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </div>
-  );
+	const brands = BRANDS_BY_COUNTRY[country] || BRANDS_BY_COUNTRY['default'];
+	return (
+		<div className='swiper-container'>
+			<Swiper
+				pagination
+				a11y={{ enabled: true }}
+				slidesPerView={3}
+				breakpoints={{
+					320: {
+						slidesPerView: 1,
+					},
+					480: {
+						slidesPerView: 2,
+					},
+					770: {
+						slidesPerView: 3,
+					},
+					991: {
+						slidesPerView: 3,
+					},
+				}}
+				autoplay={{
+					delay: 2000,
+					disableOnInteraction: true,
+				}}
+			>
+				{brands?.map((brand, i) => (
+					<SwiperSlide key={i} className='w-100 bg-primary'>
+						<div className='brand-container'>
+							<Image src={brand.img} alt='img not found' width={brand.width} />
+						</div>
+					</SwiperSlide>
+				))}
+			</Swiper>
+		</div>
+	);
 };
 
 export default BrandSlider;
--- src/data/MSK/brands.ts
+++ src/data/MSK/brands.ts
@@ -1,37 +1,40 @@
 import brand1 from '../../../public/images/brands/tropos.svg';
 import brand2 from '../../../public/webp-images/brands/colmed.webp';
 import brand3 from '../../../public/images/brands/acc.svg';
+import brand4 from '../../../public/images/brands/Euneiz.svg';
 import { StaticImageData } from 'next/image';
 
 interface Brand {
-  img: StaticImageData;
-  width: number;
+	img: StaticImageData;
+	width: number;
 }
 
 interface BrandsByCountry {
-  [key: string]: Brand[];
+	[key: string]: Brand[];
 }
 
 const DEFAULT_BRANDS: Brand[] = [
-  { img: brand1, width: 200 },
-  { img: brand2, width: 350 },
-  { img: brand3, width: 200 },
+	{ img: brand1, width: 200 },
+	{ img: brand2, width: 350 },
+	{ img: brand3, width: 200 },
+	{ img: brand4, width: 200 },
 ];
 
 export const BRANDS_BY_COUNTRY: BrandsByCountry = {
-  cl: [
-    // { img: brand2, width: 350 },
-    { img: brand2, width: 350 },
-    { img: brand3, width: 200 },
-  ],
-  ar: DEFAULT_BRANDS,
-  es: DEFAULT_BRANDS,
-  ec: DEFAULT_BRANDS,
-  mx: DEFAULT_BRANDS,
-  co: DEFAULT_BRANDS,
-  ni: DEFAULT_BRANDS,
-  pe: DEFAULT_BRANDS,
-  pa: DEFAULT_BRANDS,
-  uy: DEFAULT_BRANDS,
-  cr: DEFAULT_BRANDS,
+	cl: [
+		// { img: brand2, width: 350 },
+		{ img: brand2, width: 350 },
+		{ img: brand3, width: 200 },
+		{ img: brand4, width: 200 },
+	],
+	ar: DEFAULT_BRANDS,
+	es: DEFAULT_BRANDS,
+	ec: DEFAULT_BRANDS,
+	mx: DEFAULT_BRANDS,
+	co: DEFAULT_BRANDS,
+	ni: DEFAULT_BRANDS,
+	pe: DEFAULT_BRANDS,
+	pa: DEFAULT_BRANDS,
+	uy: DEFAULT_BRANDS,
+	cr: DEFAULT_BRANDS,
 };
