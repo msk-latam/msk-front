2d588a6a74b7a6ef1fc5a3d2c76579f9f6f01353
Ariel-Eitner-MSK <arieleitner@msklatam.com>
2024-10-17 15:23:46 -0300

    fix

--- src/components/Header/categoriesDropdown/CategoriesDropdown.tsx
+++ src/components/Header/categoriesDropdown/CategoriesDropdown.tsx
@@ -151,7 +151,11 @@ const CategoriesDropdown = ({ onClickClose }: any) => {
 							<Image src={especialidadesIcon} alt='icon' width={20} height={20} />
 							<h2 className='!font-inter'>Especialidades</h2>
 						</li>
-						<div className=' lg:max-h-[52vh] overflow-y-auto  scrollbar-thin scrollbar-thumb-[#6474A6] scrollbar-track-transparent'>
+						<div
+							className={`${
+								activeCategory ? 'max-h-64' : ''
+							} lg:max-h-[52vh] overflow-y-auto scrollbar-thin scrollbar-thumb-[#6474A6] scrollbar-track-transparent`}
+						>
 							<ul className='overflow-y-scroll scrollbar-visible lg:overflow-auto'>
 								{sortedCategories.map((category, index) => (
 									<li
@@ -167,7 +171,7 @@ const CategoriesDropdown = ({ onClickClose }: any) => {
 						</div>
 
 						{activeCategory && (
-							<div className='fixed top-36 left-0 w-full h-full  bg-[#FBFAFA]  p-5 rounded-r-2xl md:absolute md:left-full md:top-0 md:w-[24vw]  xl:shadow-[0_4px_10px_rgba(0,0,0,0.3)] '>
+							<div className=' fixed top-36 left-0 w-full h-full  bg-[#FBFAFA]  p-5 rounded-r-2xl md:absolute md:left-full md:top-0 md:w-[24vw]  xl:shadow-[0_4px_10px_rgba(0,0,0,0.3)] '>
 								<div className='flex items-center mb-2  '>
 									<Image src={arrowLeft} alt='arrow' width={10} height={10} className='mr-2' />
 									<button
@@ -179,7 +183,7 @@ const CategoriesDropdown = ({ onClickClose }: any) => {
 								</div>
 
 								<h2 className='!font-inter font-bold mb-2 pt-1 pb-2  text-lg'>{activeCategory.name}</h2>
-								<ul>
+								<ul className=''>
 									{filteredCourses?.length > 0 ? (
 										filteredCourses?.slice(0, 4).map((course: any) => (
 											<li key={course.id} className='mb-2'>
