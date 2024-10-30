3b187942c2fa0044f12cf3aedba58253a5498dd6
Ariel-Eitner-MSK <arieleitner@msklatam.com>
2024-10-30 16:11:50 -0300

    disabled buttons

--- src/components/MSK/account/CoursesTable/ButtonAccessOrSignCourse.tsx
+++ src/components/MSK/account/CoursesTable/ButtonAccessOrSignCourse.tsx
@@ -7,73 +7,56 @@ import { useRouter } from 'next/navigation';
 import ButtonOffTrial from '@/components/MSK/account/CoursesTable/ButtonOffTrial';
 
 interface ButtonAccessOrSignCourseProps {
-  email: string;
-  goToEnroll: (product_code: number, email: string) => Promise<any>;
-  goToLMS: (
-    product_code: number,
-    cod_curso: string,
-    email: string,
-  ) => Promise<void>;
-  item: UserCourseProgress;
+	email: string;
+	goToEnroll: (product_code: number, email: string) => Promise<any>;
+	goToLMS: (product_code: number, cod_curso: string, email: string) => Promise<void>;
+	item: UserCourseProgress;
 }
 
-const ButtonAccessOrSignCourse: FC<ButtonAccessOrSignCourseProps> = ({
-  email,
-  goToEnroll,
-  goToLMS,
-  item,
-}) => {
-  const statusOV = statusOrdenVenta(item?.ov);
+const ButtonAccessOrSignCourse: FC<ButtonAccessOrSignCourseProps> = ({ email, goToEnroll, goToLMS, item }) => {
+	const statusOV = statusOrdenVenta(item?.ov);
 
-  const [isDisabled, setIsDisabled] = useState(statusOV.isDisabled);
+	const [isDisabled, setIsDisabled] = useState(statusOV.isDisabled);
 
-  const router = useRouter();
+	const router = useRouter();
 
-  const disabledRender = () => {
-    return (
-      <ButtonPrimary
-        onClick={() => {}}
-        sizeClass='py-0.5 sm:py-1 px-2 sm:px-5'
-        disabled={true}
-      >
-        <span className='text-[14px] sm:text-sm font-bold'>Activar</span>
-      </ButtonPrimary>
-    );
-  };
+	const disabledRender = () => {
+		return (
+			<ButtonPrimary onClick={() => {}} sizeClass='py-0.5 sm:py-1 px-2 sm:px-5' disabled={true}>
+				<span className='text-[14px] sm:text-sm font-bold'>Activar</span>
+			</ButtonPrimary>
+		);
+	};
 
-  if (statusOV.hasText === 'Preventa') {
-    return disabledRender();
-  }
+	if (statusOV.hasText === 'Preventa' || statusOV.hasText === 'Baja') {
+		return disabledRender();
+	}
 
-  return (
-    <>
-      {isDisabled ? (
-        <ButtonPrimary
-          onClick={() => router.push(`/curso/${item.slug}`)}
-          sizeClass='py-0.5 sm:py-1 px-2 sm:px-5'
-        >
-          <span className='text-[14px] sm:text-sm font-bold'>Inscríbete</span>
-        </ButtonPrimary>
-      ) : (
-        <>
-          <div className='flex gap-2 md:block'>
-            <ButtonAccessCourse
-              email={email}
-              goToEnroll={goToEnroll}
-              goToLMS={goToLMS}
-              item={item}
-            />
+	return (
+		<>
+			{isDisabled ? (
+				<ButtonPrimary
+					disabled={isDisabled}
+					onClick={() => router.push(`/curso/${item.slug}`)}
+					sizeClass='py-0.5 sm:py-1 px-2 sm:px-5'
+				>
+					<span className='text-[14px] sm:text-sm font-bold'>Inscríbete</span>
+				</ButtonPrimary>
+			) : (
+				<>
+					<div className='flex gap-2 md:block'>
+						<ButtonAccessCourse email={email} goToEnroll={goToEnroll} goToLMS={goToLMS} item={item} />
 
-            {item.ov === 'Trial' && (
-              <div className=''>
-                <ButtonOffTrial item={item} email={email} />
-              </div>
-            )}
-          </div>
-        </>
-      )}
-    </>
-  );
+						{item.ov === 'Trial' && (
+							<div className=''>
+								<ButtonOffTrial item={item} email={email} />
+							</div>
+						)}
+					</div>
+				</>
+			)}
+		</>
+	);
 };
 
 export default ButtonAccessOrSignCourse;
