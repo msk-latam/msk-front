9a4846b2d838288fea6d794193df2647ff215879
Ariel-Eitner-MSK <arieleitner@msklatam.com>
2024-12-02 12:04:25 -0300

    footer fix, home brands added

--- src/components/MSK/BrandSlider.tsx
+++ src/components/MSK/BrandSlider.tsx
@@ -1,8 +1,6 @@
 'use client';
 import React, { FC } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
 import { BRANDS_BY_COUNTRY } from '@/data/MSK/brands';
-import 'swiper/css/bundle';
 import Image from 'next/image';
 
 interface BrandSliderProps {
@@ -13,46 +11,16 @@ const BrandSlider: FC<BrandSliderProps> = ({ country }) => {
 	const brands = BRANDS_BY_COUNTRY[country] || BRANDS_BY_COUNTRY['default'];
 
 	return (
-		<div>
-			{/* Slider visible solo en mobile */}
-			<div className='block lg:hidden swiper-container'>
-				<Swiper
-					pagination
-					a11y={{ enabled: true }}
-					slidesPerView={3}
-					breakpoints={{
-						320: {
-							slidesPerView: 1,
-						},
-						480: {
-							slidesPerView: 2,
-						},
-						770: {
-							slidesPerView: 3,
-						},
-					}}
-					autoplay={{
-						delay: 2000,
-						disableOnInteraction: true,
-					}}
-				>
-					{brands?.map((brand, i) => (
-						<SwiperSlide key={i} className='w-100 bg-primary'>
-							<div className='brand-container'>
-								<Image src={brand.img} alt='img not found' width={brand.width} />
-							</div>
-						</SwiperSlide>
+		<div className='relative'>
+			<div className='overflow-x-auto flex space-x-8 py-4 scrollbar-thin'>
+				{/* Contenedor con scroll horizontal */}
+				<div className='flex items-center space-x-8'>
+					{brands?.map((brand, index) => (
+						<div key={index} className='flex-shrink-0'>
+							<Image src={brand.img} alt='Brand logo' width={brand.width} height={100} className='object-contain' />
+						</div>
 					))}
-				</Swiper>
-			</div>
-
-			{/* Grid visible solo en desktop */}
-			<div className='hidden lg:grid grid-cols-4 gap-4'>
-				{brands?.map((brand, i) => (
-					<div key={i} className=' flex justify-center py-2'>
-						<Image src={brand.img} alt='img not found' width={brand.width} />
-					</div>
-				))}
+				</div>
 			</div>
 		</div>
 	);
