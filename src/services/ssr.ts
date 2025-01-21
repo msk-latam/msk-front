0e0a6ac978934991ba28ba3cce6ff44617be8518
Ariel-Eitner-MSK <arieleitner@msklatam.com>
2025-01-21 15:10:38 -0300

    environment

--- src/services/ssr.ts
+++ src/services/ssr.ts
@@ -217,7 +217,8 @@ class ApiSSRService {
 			const countryFile = validCountries.includes(country || '') ? `${country}.json` : `int.json`;
 
 			// Construir la URL para el archivo JSON
-			const fileUrl = `/products/${countryFile}`;
+			const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || '';
+			const fileUrl = `${baseUrl}/products/${countryFile}`;
 
 			// Obtener los datos desde el archivo JSON
 			const response = await fetch(fileUrl);
