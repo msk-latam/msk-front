134a37ac0b18004f96878a733318e176b9c323a3
Ariel-Eitner-MSK <arieleitner@msklatam.com>
2025-01-22 15:57:05 -0300

    i18n config

--- src/i18nConfig.js
+++ src/i18nConfig.js
@@ -1,25 +1,37 @@
-import ssr from "/src/services/ssr";
-import { countries } from "@/data/countries";
+import ssr from '/src/services/ssr';
+import { countries } from '@/data/countries';
 
 let locales = countries.map((item) => item.id);
-locales.push("int");
+locales.push('int');
+
+const getCountryCodeWithTimeout = (timeout = 3000) =>
+	Promise.race([
+		ssr.getCountryCode(),
+		new Promise((_, reject) => setTimeout(() => reject(new Error('Timeout fetching country code')), timeout)),
+	]);
 
 const i18nConfig = {
-  locales: locales,
-  defaultLocale: "int",
-  serverSetCookie: "always",
-  localeDetector: async (request, config) => {
-    //If the cookie NEXT_LOCALE is set, use that, if not query the API and set the cookie
-    let cookie = request.cookies.get("NEXT_LOCALE");
-    if (cookie) {
-      return cookie.value;
-    } else {
-      let countryCode = await ssr.getCountryCode();
-      // console.log('Country0', countryCode);
-      request.cookies.set("NEXT_LOCALE", countryCode);
-      return countryCode;
-    }
-  },
+	locales: locales,
+	defaultLocale: 'int',
+	serverSetCookie: 'always',
+	localeDetector: async (request, config) => {
+		let cookie = request.cookies.get('NEXT_LOCALE');
+		if (cookie && locales.includes(cookie.value)) {
+			return cookie.value;
+		} else {
+			try {
+				let countryCode = await getCountryCodeWithTimeout();
+				if (locales.includes(countryCode)) {
+					request.cookies.set('NEXT_LOCALE', countryCode);
+					return countryCode;
+				} else {
+					return config.defaultLocale;
+				}
+			} catch {
+				return config.defaultLocale; // Default en caso de error o timeout
+			}
+		}
+	},
 };
 
 module.exports = i18nConfig;
