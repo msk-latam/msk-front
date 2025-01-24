807920b17c03151bc5797ec67f92122dcb6244b3
Ariel-Eitner-MSK <arieleitner@msklatam.com>
2025-01-24 15:34:32 -0300

    rutas json para vp

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -2,41 +2,40 @@ import { Product } from '@/data/apiTypes/productTypes';
 import { NextResponse } from 'next/server';
 
 export const GET = async (req: Request) => {
-  const { searchParams } = new URL(req.url);
-  const countryParam = searchParams.get('country');
+	const { searchParams } = new URL(req.url);
+	const countryParam = searchParams.get('country');
 
-  try {
-    const response = await fetch(
-      `https://wp.msklatam.com/wp-json/wp/api/products?limit=-1&filter=all&country=${countryParam}&vp`,
-    );
+	try {
+		const response = await fetch(`https://msklatam.com/products/${countryParam}.json`);
+		// const response = await fetch(
+		//   `https://wp.msklatam.com/wp-json/wp/api/products?limit=-1&filter=all&country=${countryParam}&vp`,
+		// );
 
-    if (!response.ok) {
-      throw new Error(`Failed to fetch data from the API`);
-    }
+		if (!response.ok) {
+			throw new Error(`Failed to fetch data from the API`);
+		}
 
-    const data = await response.json();
-    const { products } = data;
+		const data = await response.json();
+		const { products } = data;
 
-    const transformedProducts = products.map((product: Product) => {
-      const totalPriceNumber = parseFloat(
-        product.total_price.replace(/\./g, ''),
-      );
+		const transformedProducts = products.map((product: Product) => {
+			const totalPriceNumber = parseFloat(product.total_price.replace(/\./g, ''));
 
-      return {
-        ...product,
-        price: totalPriceNumber,
-        tags: product.professions,
-        horas: product.duration,
-      };
-    });
+			return {
+				...product,
+				price: totalPriceNumber,
+				tags: product.professions,
+				horas: product.duration,
+			};
+		});
 
-    const res = NextResponse.json(transformedProducts);
+		const res = NextResponse.json(transformedProducts);
 
-    return res;
-  } catch (error) {
-    console.error('Error en el endpoint API:', error);
-    const res = NextResponse.json({ error: 'Error interno del servidor' });
+		return res;
+	} catch (error) {
+		console.error('Error en el endpoint API:', error);
+		const res = NextResponse.json({ error: 'Error interno del servidor' });
 
-    return res;
-  }
+		return res;
+	}
 };
