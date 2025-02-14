87f52e62c38bb25d809d2000742e4b70771a5fc2
Ariel-Eitner-MSK <arieleitner@msklatam.com>
2025-02-14 16:56:25 -0300

    checkout para argentina

--- src/app/checkout/[curso]/page.tsx
+++ src/app/checkout/[curso]/page.tsx
@@ -12,7 +12,7 @@ type Props = {
 };
 
 const CursoCheckout: FC<Props> = async ({ params }) => {
-	const { product } = await ssr.getSingleProduct(params.curso, params.lang);
+	const { product } = await ssr.getSingleProduct(params.curso, 'ar');
 
 	return (
 		<CheckoutProvider>
@@ -22,10 +22,10 @@ const CursoCheckout: FC<Props> = async ({ params }) => {
 					<CheckoutHeader />
 					<div className='flex flex-col md:flex-row gap-6'>
 						<div className='flex-1'>
-							<CheckoutContent product={product} country={params.lang} />
+							<CheckoutContent product={product} country={'ar'} />
 						</div>
 						<div className='w-full md:w-1/3'>
-							<CheckoutResume product={product} country={params.lang} />
+							<CheckoutResume product={product} country={'ar'} />
 						</div>
 					</div>
 				</div>
