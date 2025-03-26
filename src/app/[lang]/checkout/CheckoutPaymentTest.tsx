5a4f63f0b118699fc1b188fb2a20041d865232b9
Ariel-Eitner-MSK <arieleitner@msklatam.com>
2025-03-26 16:07:52 -0300

    cambio en rebill

--- src/app/[lang]/checkout/CheckoutPaymentTest.tsx
+++ src/app/[lang]/checkout/CheckoutPaymentTest.tsx
@@ -125,7 +125,7 @@ const CheckoutRebill: React.FC<CheckoutRebillProps> = ({ mode = 'payment', count
 					const data = await createPaymentRebill(
 						formData.customerData?.email,
 						contract_id,
-						formData.amount,
+						transactionAmountWithDiscount,
 						formData.currency,
 						e.card.id,
 						country,
