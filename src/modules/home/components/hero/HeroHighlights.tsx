0c3210a9e0c7cf51f76532385229e155f7cdaf21
Sol <sol6801@gmail.com>
2025-04-04 10:15:10 -0300

    Fix side space in hero highlights

--- src/modules/home/components/hero/HeroHighlights.tsx
+++ src/modules/home/components/hero/HeroHighlights.tsx
@@ -24,21 +24,21 @@ const HeroHighlight = () => {
     const container = containerRef.current;
     if (container) {
       const scrollTo = container.children[active] as HTMLElement;
-      container.scrollTo({ left: scrollTo.offsetLeft - 16, behavior: 'smooth' });
+      container.scrollTo({ left: scrollTo.offsetLeft, behavior: 'smooth' });
     }
   }, [active]);
 
   return (
-    <div className="absolute bottom-0 md:bottom-8 w-fit h-40 md:h-auto max-w-[1500px] md:w-fit portrait:-z-10 overflow-scroll md:overflow-hidden scrollbar-track-transparent">
+    <div className="md:pr-28 absolute bottom-0 md:bottom-8 w-fit h-40 md:h-auto max-w-[1200px] md:max-w-full portrait:-z-10 overflow-scroll md:overflow-hidden scrollbar-track-transparent">
       <div
         ref={containerRef}
-        className="rounded-t-xl w-auto md:w-full flex flex-col md:flex-row md:gap-[2px] overflow-y-hidden overflow-x-hidden justify-start items-center scroll-smooth gap-0 md:no-scrollbar"
+        className="rounded-t-xl w-fit md:max-w-full flex flex-col md:flex-row md:gap-[2px] overflow-y-hidden overflow-x-hidden justify-start items-center scroll-smooth gap-0 md:no-scrollbar"
       >
         {highlights.map((text, i) => (
           <button
             key={i}
             onClick={() => setActive(i)}
-            className={`w-full md:w-[22%] min-h-10 md:h-[70px] p-4 md:py-[10px] flex items-start md:items-center justify-between text-left text-m md:text-xs md:whitespace-pre-line border-white md:backdrop-blur-sm transition-all duration-300 shrink-0 ${
+            className={`w-full md:w-1/4 min-h-10 md:h-[70px] p-4 md:py-[10px] flex items-start md:items-center justify-between text-left text-m md:text-xs md:whitespace-pre-line border-white md:backdrop-blur-sm transition-all duration-300 shrink-0 ${
               active === i ? 'bg-white text-black shadow-md' : 'md:bg-black/10 md:text-white bg-white text-gray-500'
             }`}
           >
