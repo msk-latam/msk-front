ee70375990852538f838bd8b5d3c0874d94ff5e4
Ariel-Eitner-MSK <arieleitner@msklatam.com>
2025-04-14 10:07:09 -0300

    botn fixed

--- src/app/[lang]/checkout/[curso]/page.tsx
+++ src/app/[lang]/checkout/[curso]/page.tsx
@@ -15,12 +15,12 @@ const CursoCheckout: FC<Props> = async ({ params }) => {
 	const { product } = await ssr.getSingleProduct(params.curso, params.lang);
 
 	return (
-		<CheckoutProvider>
-			<div className='min-h-screen flex flex-col'>
+		<CheckoutProvider product={product}>
+			<div className='flex flex-col min-h-screen'>
 				<div>
 					<CheckoutMSKHeader />
 					<CheckoutHeader />
-					<div className='flex flex-col md:flex-row gap-6'>
+					<div className='flex flex-col gap-6 md:flex-row'>
 						<div className='flex-1'>
 							<CheckoutContent product={product} country={params.lang} />
 						</div>
