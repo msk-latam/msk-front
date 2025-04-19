e98c1df7fd6ba296d4c17fd3b1bd062a9ecfc564
Sol <sol6801@gmail.com>
2025-04-19 19:46:57 +0200

    Fixed course overview

--- src/modules/products/components/CourseOverview.tsx
+++ src/modules/products/components/CourseOverview.tsx
@@ -1,66 +1,75 @@
-import React from 'react';
-import { CheckCircle, Star, Smile } from 'lucide-react';
-import Image from 'next/image';
+import React from "react";
+import { CheckCircle, Star, Smile } from "lucide-react";
+import { GoShieldCheck } from "react-icons/go";
+import { BiLike } from "react-icons/bi";
+import { PiCertificateLight } from "react-icons/pi";
+import Image from "next/image";
+import { ChevronDown, ChevronRight } from "react-feather";
 
 const tags = [
-  'Estabilización crítica',
-  'Diagnóstico rápido',
-  'Control de complicaciones',
-  'Seguridad del paciente',
-  'Tecnología emergencias',
-  'Planificación trauma',
-  'Gestión de riesgos',
-  'Comunicación efectiva',
+  "Estabilización crítica",
+  "Diagnóstico rápido",
+  "Control de complicaciones",
+  "Seguridad del paciente",
+  "Tecnología emergencias",
+  "Planificación trauma",
+  "Gestión de riesgos",
+  "Comunicación efectiva",
 ];
 
 const steps = [
   {
-    step: 'Inscribite',
-    icon: '/icons/course/overview/step1.svg',
+    step: "Inscribite",
+    icon: "/icons/course/overview/step1.svg",
   },
   {
-    step: 'Aprende con material actualizado por expertos',
-    icon: '/icons/course/overview/step2.svg',
+    step: "Aprende con material actualizado por expertos",
+    icon: "/icons/course/overview/step2.svg",
   },
   {
-    step: 'Obtén tu certificado',
-    icon: '/icons/course/overview/step3.svg',
+    step: "Obtén tu certificado",
+    icon: "/icons/course/overview/step3.svg",
   },
   {
-    step: 'Aplica lo aprendido a tu práctica y mejora tu futuro profesional',
-    icon: '/icons/course/overview/step4.svg',
+    step: "Aplica lo aprendido a tu práctica y mejora tu futuro profesional",
+    icon: "/icons/course/overview/step4.svg",
   },
 ];
 
 const courseSteps = [
-  'Lectura de módulos teóricos',
-  'Autoevaluación al finalizar cada módulo',
-  'Clases interactivas con mirada práctica aplicada a casos clínicos',
-  'Examen final',
+  "Lectura de módulos teóricos",
+  "Autoevaluación al finalizar cada módulo",
+  "Clases interactivas con mirada práctica aplicada a casos clínicos",
+  "Examen final",
 ];
 
 const features = [
   {
-    icon: <CheckCircle className="text-pink-600 w-5 h-5" />,
-    title: '¡Obtén tu certificado digital!',
-    description: 'Al finalizar y aprobar, descarga tu certificado y compártelo con colegas en LinkedIn.',
+    icon: <PiCertificateLight className="text-[#9200AD] w-5 h-5" />,
+    title: "¡Obtén tu certificado digital!",
+    description:
+      "Al finalizar y aprobar, descarga tu certificado y compártelo con colegas en LinkedIn.",
   },
   {
-    icon: <Star className="text-purple-600 w-5 h-5" />,
-    title: 'El 97% recomienda este curso',
-    description: 'Este curso ha sido altamente valorado por profesionales de la salud por su calidad y aplicabilidad práctica.',
+    icon: <BiLike className="text-[#9200AD] w-5 h-5" />,
+    title: "El 97% recomienda este curso",
+    description:
+      "Este curso ha sido altamente valorado por profesionales de la salud por su calidad y aplicabilidad práctica.",
   },
   {
-    icon: <Smile className="text-pink-600 w-5 h-5" />,
-    title: 'Compromiso de satisfacción',
-    description: 'Si no deseas continuar con tu cursada dentro de los primeros 30 días, te devolvemos el 100% de tu inversión.',
+    icon: <GoShieldCheck className="text-[#9200AD] w-5 h-5" />,
+    title: "Compromiso de satisfacción",
+    description:
+      "Si no deseas continuar con tu cursada dentro de los primeros 30 días, te devolvemos el 100% de tu inversión.",
   },
 ];
 
 const CourseOverview: React.FC = () => {
   return (
     <section className="py-6 max-w-5xl mx-auto">
-      <h2 className="text-3xl font-medium font-raleway mb-4 text-center md:text-left">Desarróllate en lo importante</h2>
+      <h2 className="text-3xl font-medium font-raleway mb-4 text-center md:text-left">
+        Desarróllate en lo importante
+      </h2>
 
       {/* Tags */}
       <div className="flex flex-wrap gap-4 justify-center items-center mb-8">
@@ -75,24 +84,43 @@ const CourseOverview: React.FC = () => {
       </div>
 
       {/* Step by step */}
-      <p className="text-center md:whitespace-nowrap md:text-left text-lg font-medium mb-6">
-        Con este curso, mejorarás tu criterio clínico y te convertirás en referente en <strong>Emergentología</strong>
-      </p>
-      <div className="flex flex-col sm:flex-row items-center justify-center gap-6 mb-10 text-sm text-center text-[#29324f]">
-        {steps.map((item, idx) => (
-          <div key={idx} className="flex items-center gap-2">
-            <div className='bg-[#f7f9ff] rounded-full'>
-            <Image src={item.icon} alt="" width={30} height={30} />
-            </div>
-            <span>{item.step}</span>
-            {/* {idx < steps.length -1 && <span className="text-gray-400">➔</span>} */}
-          </div>
-        ))}
+      <div className="flex flex-col md:flex-row md:flex-nowrap md:items-center md:justify-center md:gap-4 mb-10 text-sm text-[#29324f] w-full">
+  {steps.map((item, idx) => (
+    <div key={idx} className="flex items-start md:items-center gap-3 text-left">
+      
+      {/* Columna: icono + chevron */}
+      <div className="flex flex-col items-center">
+        <div className="bg-[#f7f9ff] rounded-full w-16 h-16 flex items-center justify-center shrink-0">
+          <Image src={item.icon} alt="" width={24} height={24} />
+        </div>
+
+        {/* Chevron Down solo en mobile */}
+        {idx < steps.length - 1 && (
+          <span className="text-gray-400 block md:hidden mt-1">
+            <ChevronDown />
+          </span>
+        )}
       </div>
+
+      {/* Texto al lado del icono */}
+      <span className="font-raleway font-bold text-base md:text-sm leading-tight pt-4 md:pt-0">{item.step}</span>
+
+      {/* Chevron Right solo en desktop */}
+      {idx < steps.length - 1 && (
+        <span className="text-gray-400 hidden md:block">
+          <ChevronRight />
+        </span>
+      )}
+    </div>
+  ))}
+</div>
+
 
       {/* Paso a paso */}
       <div className="bg-[#f7f9ff] rounded-[38px] p-6 mb-10">
-        <h3 className='pb-6 font-raleway text-[18px] font-medium text-center'>Tu cursada paso a paso</h3>
+        <h3 className="pb-6 font-raleway text-[18px] font-medium md:text-left text-center">
+          Tu cursada, paso a paso
+        </h3>
         <div className="grid gap-6 md:grid-cols-2 text-sm text-[#29324f] text-left font-inter">
           {courseSteps.map((step, idx) => (
             <div key={idx} className="flex items-start gap-3">
@@ -104,14 +132,14 @@ const CourseOverview: React.FC = () => {
       </div>
 
       {/* Cards informativas */}
-      <div className="grid gap-6 md:grid-cols-3 text-center">
+      <div className="grid gap-4 md:grid-cols-3 text-center">
         {features.map((item, index) => (
           <div
             key={index}
-            className="rounded-xl border border-gray-200 p-6 shadow-sm bg-white"
+            className="rounded-xl border border-gray-200 py-6 px-2 shadow-sm bg-white"
           >
-            <div className="flex justify-center mb-3">{item.icon}</div>
-            <h3 className="font-semibold text-md mb-2">{item.title}</h3>
+            <div className="flex justify-start pl-2 mb-3">{item.icon}</div>
+            <h3 className="font-semibold text-md mb-2 break-words w-full">{item.title}</h3>
             <p className="text-sm text-gray-600">{item.description}</p>
           </div>
         ))}
