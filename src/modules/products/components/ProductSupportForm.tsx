295f8a81a2e610f1d1568e2823b2ee39a4ec566f
Sol <sol6801@gmail.com>
2025-04-19 23:28:34 +0200

    Fix button size in form in desktop view

--- src/modules/products/components/ProductSupportForm.tsx
+++ src/modules/products/components/ProductSupportForm.tsx
@@ -53,7 +53,7 @@ export default function ProductSupportForm(props: ProductSupportFormProps) {
         </div>
 
         <div className="col-span-1 md:col-span-2 text-right">
-          <button type="submit" className="bg-[#9200AD] hover:bg-[#6b1679] text-white px-6 py-2 rounded-full flex md:flex-row md:place-self-end md:gap-1 w-full justify-center gap-2">
+          <button type="submit" className="bg-[#9200AD] hover:bg-[#6b1679] text-white px-6 py-2 rounded-full flex md:flex-row md:place-self-end md:gap-1 w-full md:w-fit justify-center gap-2">
             Enviar
             <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M10.4995 14.0016L20.9995 3.50164M10.6271 14.3297L13.2552 21.0877C13.4867 21.6831 13.6025 21.9807 13.7693 22.0676C13.9139 22.143 14.0862 22.1431 14.2308 22.0679C14.3977 21.9812 14.5139 21.6837 14.7461 21.0886L21.3364 4.20083C21.5461 3.66366 21.6509 3.39507 21.5935 3.22344C21.5437 3.07439 21.4268 2.95742 21.2777 2.90763C21.1061 2.85029 20.8375 2.95511 20.3003 3.16474L3.41258 9.75508C2.8175 9.9873 2.51997 10.1034 2.43326 10.2703C2.35809 10.415 2.35819 10.5873 2.43353 10.7319C2.52043 10.8987 2.81811 11.0144 3.41345 11.2459L10.1715 13.8741C10.2923 13.9211 10.3527 13.9446 10.4036 13.9808C10.4487 14.013 10.4881 14.0525 10.5203 14.0975C10.5566 14.1484 10.5801 14.2089 10.6271 14.3297Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
