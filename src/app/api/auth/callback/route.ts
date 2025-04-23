b6ad4d2ee46eefb0751353149636d35caa4617ed
Agustin Mouriño - Personal <agus442m@gmail.com>
2025-04-23 18:07:48 -0300

    fix callback

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -1,6 +1,6 @@
 // /app/api/auth/callback/route.ts
-import { handleCallback } from '@/lib/auth0';
+import { auth0 } from '@/lib/auth0';
 
 export const dynamic = 'force-dynamic';
 
-export const GET = handleCallback;
+export const GET = auth0.handleCallback;
