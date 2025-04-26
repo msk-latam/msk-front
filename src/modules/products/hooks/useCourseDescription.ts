b02644730d8db33298d4e817d8c2e10f952c61d3
Sol <sol6801@gmail.com>
2025-04-26 18:43:23 +0200

    fix api call structure en useCourseDescription

--- src/modules/products/hooks/useCourseDescription.ts
+++ src/modules/products/hooks/useCourseDescription.ts
@@ -1,30 +1,28 @@
-import axios from 'axios';
+
 import { useEffect, useState } from 'react';
 import { CourseDescription } from '../types/types';
-const API_BASE = 'https://cms1.msklatam.com/wp-json/msk/v1/product';
+import { getCourse } from '../service/courseService'
 
-export function useCourseDescription(slug: string | number) {
+export function useCourseDescription(slug: string) {
 	const [data, setData] = useState<CourseDescription | null>(null);
 	const [loading, setLoading] = useState(true);
 	const [error, setError] = useState<string | null>(null);
-
+  
 	useEffect(() => {
-		const fetchData = async () => {
-			setLoading(true);
-			try {
-				const res = await axios.get(`${API_BASE}/${slug}`);
-				setData(res.data.sections.content);
-			} catch (err) {
-				setError('Error al cargar la descripción del curso');
-				console.error(err);
-			} finally {
-				setLoading(false);
-			}
-		};
-
-		fetchData();
+	  if (!slug) return;
+  
+	  getCourse(slug)
+		.then((courseData) => {
+		  // courseData es todo el objeto del curso
+		  const headerData: CourseDescription = courseData.sections.content;
+		  setData(headerData);
+		})
+		.catch((err) => {
+		  console.error(err);
+		  setError(err.message || "Error fetching header data");
+		})
+		.finally(() => setLoading(false));
 	}, [slug]);
-	console.log(data);
-
+	
 	return { data, loading, error };
 }
