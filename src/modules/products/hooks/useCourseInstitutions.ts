51e249824af58b8455c9bb9751726d5f7926665c
Sol <sol6801@gmail.com>
2025-04-26 18:47:50 +0200

    Fix api call structure en useCourseInstitutions

--- src/modules/products/hooks/useCourseInstitutions.ts
+++ src/modules/products/hooks/useCourseInstitutions.ts
@@ -1,37 +1,28 @@
-import axios from 'axios';
+
 import { useEffect, useState } from 'react';
+import { CourseInstitution } from '../types/types';
+import { getCourse } from '../service/courseService'
 
-const API_BASE = 'https://cms1.msklatam.com/wp-json/msk/v1/product';
-
-export interface CourseInstitution {
-	id: number;
-	title: string;
-	slug: string;
-	image: string;
-	description: string;
-}
-
-export function useCourseInstitutions(slug: string | number) {
-	const [data, setData] = useState<CourseInstitution[] | null>(null);
+export function useCourseInstitutions(slug: string) {
+	const [data, setData] = useState<CourseInstitution | null>(null);
 	const [loading, setLoading] = useState(true);
 	const [error, setError] = useState<string | null>(null);
-
+  
 	useEffect(() => {
-		async function fetchInstitutions() {
-			setLoading(true);
-			try {
-				const res = await axios.get(`${API_BASE}/${slug}?nocache=1`);
-				setData(res.data.sections.institutions);
-			} catch (err) {
-				console.error(err);
-				setError('Error fetching course institutions');
-			} finally {
-				setLoading(false);
-			}
-		}
-
-		fetchInstitutions();
+	  if (!slug) return;
+  
+	  getCourse(slug)
+		.then((courseData) => {
+		  // courseData es todo el objeto del curso
+		  const headerData: CourseInstitution = courseData.sections.institutions;
+		  setData(headerData);
+		})
+		.catch((err) => {
+		  console.error(err);
+		  setError(err.message || "Error fetching institutions data");
+		})
+		.finally(() => setLoading(false));
 	}, [slug]);
-
+	
 	return { data, loading, error };
 }
