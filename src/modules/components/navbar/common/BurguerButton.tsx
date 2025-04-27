e9b7ca72f2adf816468d7a25fb18b6814121fdc5
Agustin Mouriño - Personal <agus442m@gmail.com>
2025-04-27 15:05:08 -0300

    Ajuste a boton de navbar

--- src/modules/components/navbar/common/BurguerButton.tsx
+++ src/modules/components/navbar/common/BurguerButton.tsx
@@ -1,39 +1,23 @@
 // components/BurgerButton.tsx
 interface BurgerButtonProps {
-    isOpen: boolean;
-    onClick: () => void;
-    color?: string;
-    uniformWidth?: boolean;
-  }
-  
-  export const BurgerButton = ({
-    isOpen,
-    onClick,
-    color = "white",
-    uniformWidth = false,
-  }: BurgerButtonProps) => {
-    const commonClass = `absolute h-0.5 transition-all duration-300 ease-in-out`;
-    const style = { backgroundColor: color };
-  
-    return (
-      <button
-        aria-label="Menú"
-        className="relative w-6 h-6 flex items-center justify-center z-50"
-        onClick={onClick}
-      >
-        <span
-          className={`${commonClass} w-5 ${isOpen ? "rotate-45 translate-y-0" : "-translate-y-2"}`}
-          style={style}
-        />
-        <span
-          className={`${commonClass} ${isOpen ? "opacity-0" : "opacity-100"} ${uniformWidth ? "w-5" : "w-3"} left-[2px]`}
-          style={style}
-        />
-        <span
-          className={`${commonClass} w-5 ${isOpen ? "-rotate-45 translate-y-0" : "translate-y-2"}`}
-          style={style}
-        />
-      </button>
-    );
-  };
-  
+	isOpen: boolean;
+	onClick: () => void;
+	color?: string;
+	uniformWidth?: boolean;
+}
+
+export const BurgerButton = ({ isOpen, onClick, color = 'white', uniformWidth = false }: BurgerButtonProps) => {
+	const commonClass = `absolute h-[1.8px] rounded-full transition-all duration-300 ease-in-out`;
+	const style = { backgroundColor: color };
+
+	return (
+		<button aria-label='Menú' className='relative w-6 h-6 flex items-center justify-center z-50' onClick={onClick}>
+			<span className={`${commonClass} w-5 ${isOpen ? 'rotate-45 translate-y-0' : '-translate-y-2'}`} style={style} />
+			<span
+				className={`${commonClass} ${isOpen ? 'opacity-0' : 'opacity-100'} ${uniformWidth ? 'w-5' : 'w-3'} left-[2px]`}
+				style={style}
+			/>
+			<span className={`${commonClass} w-5 ${isOpen ? '-rotate-45 translate-y-0' : 'translate-y-2'}`} style={style} />
+		</button>
+	);
+};
