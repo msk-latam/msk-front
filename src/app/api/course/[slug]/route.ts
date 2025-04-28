0d86af63f2b1a105c02fd68ccefed011aa54fc43
Sol <sol6801@gmail.com>
2025-04-28 16:03:41 +0200

    Change revalidate 0 to 30

--- src/app/api/course/[slug]/route.ts
+++ src/app/api/course/[slug]/route.ts
@@ -1,20 +1,4 @@
-// // app/api/course/[slug]/route.ts
-// import { NextResponse } from 'next/server';
 
-// export async function GET(req: Request, { params }: { params: { slug: string } }) {
-//   const { slug } = params;
-
-//   try {
-//     const res = await fetch(`https://cms1.msklatam.com/wp-json/msk/v1/course/${slug}`);
-//     const data = await res.json();
-//     return NextResponse.json(data);
-//   } catch (error) {
-//     console.error('Error in route handler:', error);
-//     return NextResponse.json({ message: 'Error fetching course' }, { status: 500 });
-//   }
-// }
-
-// app/api/course/[slug]/route.ts
 import { NextResponse } from 'next/server';
 
 export async function GET(req: Request, { params }: { params: { slug: string } }) {
@@ -22,7 +6,7 @@ export async function GET(req: Request, { params }: { params: { slug: string } }
 
 	try {
 		const res = await fetch(`https://cms1.msklatam.com/wp-json/msk/v1/product/${slug}`, {
-			next: { revalidate: 0 },
+			next: { revalidate: 30 },
 		});
 		const data = await res.json();
 		return NextResponse.json(data);
