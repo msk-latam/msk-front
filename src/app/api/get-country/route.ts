7ecf45aae0e3418dadb5a81d7798240ba332ae02
ascazzino <agustinscazzino@gmail.com>
2025-04-29 23:51:29 -0300

    fixed api to detect ip to prevent vercel ip

--- src/app/api/get-country/route.ts
+++ src/app/api/get-country/route.ts
@@ -1,10 +1,39 @@
-import { NextResponse } from 'next/server';
+{/*import { NextResponse } from 'next/server';
 
 export async function GET() {
 	try {
 		const ipRes = await fetch('https://api.ipify.org?format=json');
 		const ipData = await ipRes.json();
 		const ip = ipData.ip;
+
+		const geoRes = await fetch(`https://pro.ip-api.com/json/${ip}?fields=61439&key=OE5hxPrfwddjYYP`);
+		if (!geoRes.ok) throw new Error('Failed to fetch geo info');
+
+		const geo = await geoRes.json();
+
+		return NextResponse.json({
+			ip,
+			country: geo.countryCode?.toLowerCase() || '',
+			name: geo.country || '',
+		});
+	} catch (error) {
+		console.error('❌ /api/get-country error:', error);
+		return NextResponse.json(
+			{ ip: '', country: '', name: '' },
+			{ status: 500 }
+		);
+	}
+}*/ }
+
+
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(req: NextRequest) {
+	try {
+		const forwarded = req.headers.get('x-forwarded-for');
+		const ip = forwarded?.split(',')[0] || ''; // puede haber varias IPs
+
+		if (!ip) throw new Error('No client IP detected');
 
 		const geoRes = await fetch(`https://pro.ip-api.com/json/${ip}?fields=61439&key=OE5hxPrfwddjYYP`);
 		if (!geoRes.ok) throw new Error('Failed to fetch geo info');
