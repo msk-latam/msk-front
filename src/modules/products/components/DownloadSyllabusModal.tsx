4f3d9dcc193dfbf51d8eecd7314df954ee294b5c
Sol <sol6801@gmail.com>
2025-05-02 08:26:47 +0200

    Add other profession and speciality fields to forms

--- src/modules/products/components/DownloadSyllabusModal.tsx
+++ src/modules/products/components/DownloadSyllabusModal.tsx
@@ -12,6 +12,9 @@ interface DownloadSyllabusModalProps {
 export default function DownloadSyllabusModal({ fileUrl, onClose, slug }: DownloadSyllabusModalProps) {
 	const modalRef = useRef<HTMLDivElement | null>(null);
 
+	const [profession, setProfession] = useState("");
+	const [specialty, setSpecialty] = useState("");
+  
 	const [formData, setFormData] = useState({
 		name: '',
 		lastName: '',
@@ -140,136 +143,182 @@ export default function DownloadSyllabusModal({ fileUrl, onClose, slug }: Downlo
 					</div>
 
 					{/* Profession and Speciality */}
-					<div className='flex flex-col md:flex-row gap-4'>
-						<select
-							name='profession'
-							value={formData.profession}
-							onChange={handleChange}
-							className='w-full text-gray-500 text-base rounded-2xl border border-[#DBDDE2] p-3 pl-4 focus:ring-4 focus:border-[#DBDDE2] focus:ring-[#F5E6F7]'
-						>
-							<option value=''>Seleccionar profesión</option>
-							<option value='Personal médico/1'>Personal médico</option>
-							<option value='Residente/2'>Residente</option>
-							<option value='Licenciado de la salud/3'>Licenciado de la salud</option>
-							<option value='Personal de enfermería/4'>Personal de enfermería</option>
-							<option value='Auxiliar de enfermería/5'>Auxiliar de enfermería</option>
-							<option value='Fuerza pública/6'>Fuerza pública</option>
-							<option value='Técnico universitario/7'>Técnico universitario</option>
-							<option value='Estudiante/8'>Estudiante</option>
-							<option value='Tecnología Médica/9'>Tecnología Médica</option>
-							<option value='Otra profesión/10'>Otra profesión</option>
-						</select>
+					<div className="col-span-1 md:col-span-1 space-y-3">
+          {/* Profesión */}
+          <select
+            value={profession}
+            onChange={(e) => setProfession(e.target.value)}
+            className="w-full text-gray-600 border border-[#DBDDE2] focus:outline-none focus:ring-4 focus:border-[#DBDDE2] focus:ring-[#F5E6F7] rounded-[16px] px-3 py-2"
+          >
+            <option value="">Seleccionar profesión</option>
+            <option value="Personal médico/1">Personal médico</option>
+            <option value="Residente/2">Residente</option>
+            <option value="Licenciado de la salud/3">
+              Licenciado de la salud
+            </option>
+            <option value="Personal de enfermería/4">
+              Personal de enfermería
+            </option>
+            <option value="Auxiliar de enfermería/5">
+              Auxiliar de enfermería
+            </option>
+            <option value="Fuerza pública/6">Fuerza pública</option>
+            <option value="Técnico universitario/7">
+              Técnico universitario
+            </option>
+            <option value="Estudiante/8">Estudiante</option>
+            <option value="Tecnología Médica/9">Tecnología Médica</option>
+            <option value="Otra Profesión">Otra Profesión</option>
+          </select>
+          {profession === "Otra Profesión" && (
+            <input
+              type="text"
+              placeholder="Ingresar otra profesión"
+              className="w-full border border-[#DBDDE2] rounded-[16px] px-3 py-2 focus:outline-none focus:ring-4 focus:ring-[#F5E6F7]"
+            />
+          )}
+        </div>
+        <div className="col-span-1 md:col-span-1 space-y-3">
+          {/* Especialidad */}
+          <select
+            value={specialty}
+            onChange={(e) => setSpecialty(e.target.value)}
+            className="w-full text-gray-600 border border-[#DBDDE2] focus:outline-none focus:ring-4 focus:border-[#DBDDE2] focus:ring-[#F5E6F7] rounded-[16px] px-3 py-2"
+          >
+            <option value="">Seleccionar especialidad</option>
+            <option>Alergia e inmunología</option>
+            <option>Anatomía patológica</option>
+            <option>Coloproctología</option>
+            <option>Flebología y linfología</option>
+            <option>Hepatología</option>
+            <option>Mastología</option>
+            <option>Medicina de la industria farmaceútica</option>
+            <option>Medicina del trabajo / ocupacional</option>
+            <option>Medicina estética</option>
+            <option>Medicina física y rehabilitación</option>
+            <option>Medicina legal</option>
+            <option>Medicina paliativa y dolor</option>
+            <option>Medicina reproductiva y fertilidad</option>
+            <option>Neumonología</option>
+            <option>Reumatología</option>
+            <option>Toxicología</option>
+            <option>Trasplante</option>
+            <option>Urología</option>
+            <option>Enfermería familiar y comunitaria</option>
+            <option>Enfermería en administración y gestión sanitaria</option>
+            <option>Enfermería en análisis clínicos</option>
+            <option>Enfermería en cardiología y UCO</option>
+            <option>Enfermería en cuidados intensivos de adultos</option>
+            <option>
+              Enfermería en cuidados intensivos pediátricos y neonatales
+            </option>
+            <option>Enfermería en cuidados paliativos y dolor</option>
+            <option>Enfermería en emergencias y atención primaria</option>
+            <option>Enfermería en internación domiciliaria</option>
+            <option>Enfermería en internación general</option>
+            <option>Enfermería en investigación</option>
+            <option>Enfermería en lactancia y puerperio</option>
+            <option>Enfermería en reproducción asistida</option>
+            <option>Enfermería en salud mental</option>
+            <option>Enfermería en unidades de trasplantes</option>
+            <option>Enfermería escolar</option>
+            <option>Enfermería geriátrica y gerontológica</option>
+            <option>Enfermería hematológica</option>
+            <option>Enfermería nefrológica y diálisis</option>
+            <option>Enfermería neonatal</option>
+            <option>Enfermería obstétrica y ginecológica</option>
+            <option>Enfermería oncológica</option>
+            <option>Enfermería pediátrica</option>
+            <option>Enfermería quirúrgica</option>
+            <option>Enfermería radiológica</option>
+            <option>Otras especialidades</option>
+            <option>Producción de bioimágenes</option>
+            <option>Bioquímica</option>
+            <option>Psicología</option>
+            <option>Farmacia</option>
+            <option>Instrumentación quirúrgica</option>
+            <option>Kinesiología y fisiatría</option>
+            <option>Óptica</option>
+            <option>Osteopatía</option>
+            <option>Podología</option>
+            <option>Terapia ocupacional</option>
+            <option>Otra carrera o licenciatura</option>
+            <option>Tecnicatura en laboratorio clínico</option>
+            <option>Tecnicatura en radiología e imágenes diagnósticas</option>
+            <option>Tecnicatura en atención de adicciones</option>
+            <option>Tecnicatura en optometría</option>
+            <option>Tecnicatura en hemoterapia e inmunohematología</option>
+            <option>
+              Tecnicatura en partería profesional con enfoque intercultural
+            </option>
+            <option>Tecnicatura en visita médica</option>
+            <option>Tecnicatura en cuidados geriátricos</option>
+            <option>
+              Tecnicatura en tecnología en ciencias del esteticismo
+            </option>
+            <option>Tecnicatura en ciencia y tecnología de alimentos</option>
+            <option>Tecnicatura en prácticas cardiológicas</option>
+            <option>Tecnicatura en esterilización</option>
+            <option>Tecnicatura en asistencia dental</option>
+            <option>Tecnicatura en cosmetología</option>
+            <option>Policía</option>
+            <option>Bombero</option>
+            <option>Guardavidas / Rescatista</option>
+            <option>Auditoría y administración sanitaria</option>
+            <option>Diabetes</option>
+            <option>Generalista</option>
+            <option>Medicina del deporte</option>
+            <option>Medicina familiar y comunitaria</option>
+            <option>Medicina intensiva</option>
+            <option>Medicina interna / clínica</option>
+            <option>Nutrición</option>
+            <option>Traumatología y ortopedia</option>
+            <option>Anestesiología</option>
+            <option>Diagnóstico por Imágenes</option>
+            <option>Cardiología</option>
+            <option>Cirugía</option>
+            <option>Cuidados críticos e intensivos</option>
+            <option>Dermatología</option>
+            <option>Emergentología</option>
+            <option>Endocrinología</option>
+            <option>Gastroenterología</option>
+            <option>Generalista - Clínica - Medicina interna</option>
+            <option>Geriatría y Gerontología</option>
+            <option>Ginecología</option>
+            <option>Hematología</option>
+            <option>Infectología</option>
+            <option>Internación domiciliaria y cuidados paliativos</option>
+            <option>Nefrología</option>
+            <option>Neonatología</option>
+            <option>Neurología</option>
+            <option>Nutrición y alimentación</option>
+            <option>Obstetricia</option>
+            <option>Obstetricia y Ginecología</option>
+            <option>Odontología</option>
+            <option>Oftalmología</option>
+            <option>Oncología</option>
+            <option>Ortopedia y Traumatología</option>
+            <option>Otorrinolaringología</option>
+            <option>Pediatría</option>
+            <option>Psiquiatría</option>
+            <option>Radiología</option>
+            <option>Bioanálisis Clínico-molecular</option>
+            <option>Medicina Transfusional</option>
+            <option>Imagenología</option>
+            <option>Radioterapia</option>
+            <option>Física Médica</option>
+            <option>Morfofisiopatología y Citodiagnóstico</option>
+            <option>Otra Especialidad</option>
+          </select>
+          {specialty === "Otra Especialidad" && (
+            <input
+              type="text"
+              placeholder="Ingresar otra especialidad"
+              className="w-full border border-[#DBDDE2] rounded-[16px] px-3 py-2 focus:outline-none focus:ring-4 focus:ring-[#F5E6F7]"
+            />
+          )}
+        </div>
 
-						<select
-							name='speciality'
-							value={formData.speciality}
-							onChange={handleChange}
-							className='w-full text-gray-500 text-base rounded-2xl border border-[#DBDDE2] p-3 pl-4 focus:ring-4 focus:border-[#DBDDE2] focus:ring-[#F5E6F7]'
-						>
-							<option value=''>Seleccionar especialidad</option>
-							<option>Alergia e inmunología</option>
-							<option>Anatomía patológica</option>
-							<option>Coloproctología</option>
-							<option>Flebología y linfología</option>
-							<option>Hepatología</option>
-							<option>Mastología</option>
-							<option>Medicina de la industria farmaceútica</option>
-							<option>Medicina del trabajo / ocupacional</option>
-							<option>Medicina estética</option>
-							<option>Medicina física y rehabilitación</option>
-							<option>Medicina legal</option>
-							<option>Medicina paliativa y dolor</option>
-							<option>Medicina reproductiva y fertilidad</option>
-							<option>Neumonología</option>
-							<option>Reumatología</option>
-							<option>Toxicología</option>
-							<option>Trasplante</option>
-							<option>Urología</option>
-							<option>Enfermería familiar y comunitaria</option>
-							<option>Enfermería en administración y gestión sanitaria</option>
-							<option>Enfermería en análisis clínicos</option>
-							<option>Enfermería en cardiología y UCO</option>
-							<option>Enfermería en cuidados intensivos de adultos</option>
-							<option>Enfermería en cuidados intensivos pediátricos y neonatales</option>
-							<option>Enfermería en cuidados paliativos y dolor</option>
-							<option>Enfermería en emergencias y atención primaria</option>
-							<option>Enfermería en internación domiciliaria</option>
-							<option>Enfermería en internación general</option>
-							<option>Enfermería en investigación</option>
-							<option>Enfermería en lactancia y puerperio</option>
-							<option>Enfermería en reproducción asistida</option>
-							<option>Enfermería en salud mental</option>
-							<option>Enfermería en unidades de trasplantes</option>
-							<option>Enfermería escolar</option>
-							<option>Enfermería geriátrica y gerontológica</option>
-							<option>Enfermería hematológica</option>
-							<option>Enfermería nefrológica y diálisis</option>
-							<option>Enfermería neonatal</option>
-							<option>Enfermería obstétrica y ginecológica</option>
-							<option>Enfermería oncológica</option>
-							<option>Enfermería pediátrica</option>
-							<option>Enfermería quirúrgica</option>
-							<option>Enfermería radiológica</option>
-							<option>Otras especialidades</option>
-							<option>Producción de bioimágenes</option>
-							<option>Bioquímica</option>
-							<option>Psicología</option>
-							<option>Farmacia</option>
-							<option>Instrumentación quirúrgica</option>
-							<option>Kinesiología y fisiatría</option>
-							<option>Óptica</option>
-							<option>Osteopatía</option>
-							<option>Podología</option>
-							<option>Terapia ocupacional</option>
-							<option>Otra carrera o licenciatura</option>
-							<option>Tecnicatura en laboratorio clínico</option>
-							<option>Tecnicatura en radiología e imágenes diagnósticas</option>
-							<option>Tecnicatura en atención de adicciones</option>
-							<option>Tecnicatura en optometría</option>
-							<option>Tecnicatura en hemoterapia e inmunohematología</option>
-							<option>Tecnicatura en partería profesional con enfoque intercultural</option>
-							<option>Tecnicatura en visita médica</option>
-							<option>Tecnicatura en cuidados geriátricos</option>
-							<option>Tecnicatura en tecnología en ciencias del esteticismo</option>
-							<option>Tecnicatura en ciencia y tecnología de alimentos</option>
-							<option>Tecnicatura en prácticas cardiológicas</option>
-							<option>Tecnicatura en esterilización</option>
-							<option>Tecnicatura en asistencia dental</option>
-							<option>Tecnicatura en cosmetología</option>
-							<option>Policía</option>
-							<option>Bombero</option>
-							<option>Guardavidas / Rescatista</option>
-							<option>Auditoría y administración sanitaria</option>
-							<option>Diabetes</option>
-							<option>Generalista</option>
-							<option>Medicina del deporte</option>
-							<option>Medicina familiar y comunitaria</option>
-							<option>Medicina intensiva</option>
-							<option>Medicina interna / clínica</option>
-							<option>Nutrición</option>
-							<option>Traumatología y ortopedia</option>
-							<option>Anestesiología</option>
-							<option>Diagnóstico por Imágenes</option>
-							<option>Cardiología</option>
-							<option>Cirugía</option>
-							<option>Cuidados críticos e intensivos</option>
-							<option>Dermatología</option>
-							<option>Emergentología</option>
-							<option>Endocrinología</option>
-							<option>Gastroenterología</option>
-							<option>Generalista - Clínica - Medicina interna</option>
-							<option>Geriatría y Gerontología</option>
-							<option>Ginecología</option>
-							<option>Hematología</option>
-							<option>Infectología</option>
-							<option>Oncología</option>
-							<option>Pediatría</option>
-							<option>Psiquiatría</option>
-							<option>Radiología</option>
-							<option>Reumatología</option>
-							<option>Urología</option>
-						</select>
-					</div>
 
 					{/* Message */}
 					<textarea
