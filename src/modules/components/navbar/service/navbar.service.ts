f89bdb16ec9f63f3ab74595424da347521c5120f
nahuelperezdev <nahuel@waveit.ar>
2025-05-05 22:56:11 +0000

    fix window undefined

--- src/modules/components/navbar/service/navbar.service.ts
+++ src/modules/components/navbar/service/navbar.service.ts
@@ -1,7 +1,10 @@
 export const getNavbar = async () => {
+  let lang = "ar";
+  if(window){
     const { pathname } = window.location;
     const langMatch = pathname.match(/^\/([^/]+)\//);
-    const lang = langMatch && langMatch[1].length <= 2 ? langMatch[1] : "ar";
+     lang = langMatch && langMatch[1].length <= 2 ? langMatch[1] : "ar";
+  }
     const res = await fetch(`/api/navbar?lang=${lang}`);
     if (!res.ok) throw new Error('Failed to fetch navbar data');
     return res.json();
