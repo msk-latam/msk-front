073358a0b40a2d0a970a44b9dfd095818253a9da
ascazzino <agustinscazzino@gmail.com>
2025-05-06 19:57:42 -0300

    added canonicals for productpage

--- src/app/[lang]/home/page.tsx
+++ src/app/[lang]/home/page.tsx
@@ -1,32 +1,31 @@
 import type { Metadata } from 'next';
+import { headers } from 'next/headers';
 import { getHomeMetadata } from '@/SEO/home/metaData/homeMetaData';
 import HomeWithPopUp from '@/modules/home/HomeWithPopUp';
 
 type Props = {
-  params: { lang: string };
+	params: { lang: string };
 };
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const lang = params.lang ?? 'ar';
+	const lang = params.lang ?? 'ar';
 
-  const baseURL = process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000' || 'msklatam.tech';
+	const headersList = headers();
+	const host = headersList.get('host') ?? '';
 
-  // Solo es producción si el dominio es .com
-  const isProd = baseURL.includes('msklatam.com');
+	const metadata = getHomeMetadata(lang, host);
 
-  const metadata = getHomeMetadata(lang, isProd);
-
-  return {
-    title: metadata.title,
-    description: metadata.description,
-    alternates: {
-      canonical: metadata.canonical,
-      languages: metadata.hreflangs, // ✅ viene del helper
-    },
-    robots: `${metadata.robots.index ? 'index' : 'noindex'}, ${metadata.robots.follow ? 'follow' : 'nofollow'}`,
-  };
+	return {
+		title: metadata.title,
+		description: metadata.description,
+		alternates: {
+			canonical: metadata.canonical,
+			languages: metadata.hreflangs,
+		},
+		robots: `${metadata.robots.index ? 'index' : 'noindex'}, ${metadata.robots.follow ? 'follow' : 'nofollow'}`,
+	};
 }
 
 export default function HomePage({ params }: Props) {
-  return <HomeWithPopUp lang={params.lang} />;
+	return <HomeWithPopUp lang={params.lang} />;
 }
